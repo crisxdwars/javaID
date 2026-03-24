d36f2bb037f39e76c61a2c9cb030cb35465881f7
crisxdwars <crisxdwars@gmail.com>
2026-03-24 13:41:42 +0800

    Created PaymentC_Talurong.java

---
+++ PaymentC_Talurong.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class PaymentC_Talurong {
+	public static void main(String[] args) {
+		int Total;
+		System.out.println("Else if & Switch Statement");
+		Scanner talurong = new Scanner(System.in);
+		System.out.print("Enter an Cash Amount: ");
+		int Amount = talurong.nextInt();
+              	System.out.println("    [1] Cash ------- 5% Discount");
+                System.out.println("    [2] Quarterly -- 5% Interest");
+                System.out.println("    [3] Monthly ---- 7% Interest");
+		System.out.print("Enter Mode: ");
+                int mode = talurong.nextInt();
+		
+	}
+}
